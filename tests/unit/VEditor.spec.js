46fb5e60ceafc4378b772fd88a9e3d20a75c60e2
David Royer <droyer01@gmail.com>
2018-07-31 03:49:35 -0400

    test(VEditor): Needed to import `mount` and not `shallowMount`

--- tests/unit/VEditor.spec.js
+++ tests/unit/VEditor.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import VEditor from "@/components/VEditor.vue";
 
 describe("VEditor.vue", () => {
@@ -9,7 +9,7 @@ describe("VEditor.vue", () => {
 
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(VEditor, {
+    const wrapper = mount(VEditor, {
       propsData: { msg }
     });
     expect(wrapper.text()).toMatch(msg);
