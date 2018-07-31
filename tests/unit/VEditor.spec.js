1228b06a3e6a170d2cc9e3eeee92199302492a0e
David Royer <droyer01@gmail.com>
2018-07-31 03:19:57 -0400

    test(VEditor): added render test

--- tests/unit/VEditor.spec.js
+++ tests/unit/VEditor.spec.js
@@ -2,6 +2,11 @@ import { shallowMount } from "@vue/test-utils";
 import VEditor from "@/components/VEditor.vue";
 
 describe("VEditor.vue", () => {
+  it("renders component", () => {
+    const wrapper = mount(VEditor);
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
   it("renders props.msg when passed", () => {
     const msg = "new message";
     const wrapper = shallowMount(VEditor, {
